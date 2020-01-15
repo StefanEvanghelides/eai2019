79e01de081be18ba9480d075229c7887b9288a9f
remco <r.m.van.buijtenen@student.rug.nl>
2020-01-15 12:42:12 +0100

    refactor message handler

--- WarehouseMessageHandler/main.py
+++ WarehouseMessageHandler/main.py
@@ -6,52 +6,19 @@ import stomp
 from urllib import request, parse, error
 import json
 import math
-import traceback
 
 from Connection import Connection, Listener
 from functools import partial
-
-
-def list_products(db, message, headers, queues):
-    cursor = db.cursor()
-
-    limit = message["pageSize"]
-    offset = max(0, message["page"] - 1) * limit
-
-    cursor.execute("SELECT * FROM demo LIMIT %d OFFSET %d" % (limit, offset))
-    result = cursor.fetchall()
-    cursor.execute("SELECT COUNT(*) FROM demo")
-    total_count = cursor.fetchall()[0][0]  # returns list of tuples for some reason
-
-    has_next = offset + limit < total_count
-    has_previous = offset > 0
-
-    headers = {
-        "type": "response",
-        "subject": "products",
-        "sender": "warehouse-message-handler",
-        "receiver": headers["sender"],
-    }
-
-    body = {
-        "pageInfo": {
-            "page": message["page"],
-            "pageSize": message["pageSize"],
-            "hasNextPage": has_next,
-            "hasPreviousPage": has_previous,
-            "pageCount": math.ceil(total_count / limit),
-        },
-        "products": result,
-    }
-
-    queues["message-bus"].send(
-        body=json.dumps(body), headers=headers, destination="message-bus-in"
-    )
-
+from handlers.request_handlers import list_products, create_product, create_db, seed_db
 
 if __name__ == "__main__":
     conn = psycopg2.connect(host="postgres", port=5432, user="postgres")
-    request_handlers = {"list-products": partial(list_products, conn)}
+    request_handlers = {
+        "list-products": partial(list_products, conn),
+        "create-product": partial(create_product, conn),
+        "create-database": partial(create_db, conn),
+        "seed-database": partial(seed_db)
+    }
     c = Connection(
         "queue",
         "queue",
@@ -59,15 +26,6 @@ if __name__ == "__main__":
         Listener(request_handlers=request_handlers),
         "warehouse-message-handler",
     )
-    # hosts = [("queue", 61613)]
-    # # start listeners for input channels
-    # queue = stomp.Connection(host_and_ports=hosts)
-    # queue.start()
-    # queue.connect("admin", "admin")
-    # register_at_message_bus(hosts, queue)
-    # start_message_listener(conn, hosts, queue)
-    # start_products_listener(conn, hosts)
-    # start_admin_listener(conn, hosts)
 
     while True:
         # keep app running to prevent docker from terminating
